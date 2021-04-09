3405959f725b7563693263fb5bf6da0e3183247c
Hemanth Kumar M <30370376+Hemanthkumar2112@users.noreply.github.com>
2021-04-09 10:57:31 -0400

    Create findangle.py

---
+++ findangle.py
@@ -0,0 +1,11 @@
+import math
+def angle(ab:'int',bc:'int') ->'to_find_a_angle':
+    x = math.atan2(bc,ab)
+    x = round(math.degrees(x))
+    x = str(x)+chr(176)
+    return x
+if __name__=="__main__":
+    ab = int(input())
+    bc = int(input())
+    x = angle(ab,bc)
+    print(x)
